133e08541eb76e359f63adf19c8e7bbdfbc8614f
wagneribanez2 <wagneribanez2@hotmail.com>
2019-01-30 12:54:31 -0200

    DepartmentList controller

---
+++ src/model/entities/Department.java
@@ -0,0 +1,73 @@
+package model.entities;
+
+import java.io.Serializable;
+
+public class Department implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int Id;
+	private String name;
+	
+	public Department() {
+	
+	}
+
+	public Department(int id, String name) {	
+		Id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (Id != other.Id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [Id=" + Id + ", name=" + name + "]";
+	}
+	
+	
+	
+	
+}
