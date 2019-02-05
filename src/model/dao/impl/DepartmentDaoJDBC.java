ec45c406514429d03e3dff305afc19567a3b1d26
wagneribanez2 <wagneribanez2@hotmail.com>
2019-02-05 09:49:39 -0200

    Remove department

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -89,7 +90,7 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 			ps.executeUpdate();			
 						
 		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
+			throw new DbIntegrityException(e.getMessage());
 		} finally {
 			DB.closeStatement(ps);			
 		}
